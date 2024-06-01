be842634e0d8be4ab26aed58633d9ff386c2f6a2
Imastara <sazizimastara@gmail.com>
2024-06-01 21:58:52 +0700

    learning chatting unicast

---
+++ week10/client.py
@@ -0,0 +1,21 @@
+import socket 
+
+def start_client(): 
+    # Konfigurasi host dan port server 
+    host = '127.0.0.1' 
+    port = 8000 
+    # Membuat socket client 
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    client_socket.connect((host, port)) 
+    while True: 
+        # Mengirim pesan ke server 
+        message = input("Your message: ") 
+        client_socket.send(message.encode('utf-8')) 
+        if message == 'exit': 
+            break 
+        # Menerima pesan dari server 
+        response = client_socket.recv(1024).decode('utf-8') 
+        print(f"[SERVER RESPONSE] {response}") 
+    # Menutup koneksi dengan server 
+    client_socket.close() 
+start_client() 
